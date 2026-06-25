c917f071d8b8baebf68a4a97f6357145b8b0c438
Ahmed El-Shamy <ahmedelshamy14620@gmail.com>
2026-06-25 09:59:17 +0200

    start Reward Shaping Modification Senario with DQN

--- dqn_algorithms_experiments/RewardWrapper.py
+++ dqn_algorithms_experiments/RewardWrapper.py
@@ -1,12 +1,14 @@
 import gymnasium as gym
 import numpy as np
 
-class DistancePenaltyWrapper(gym.RewardWrapper):
+class DistancePenaltyWrapper(gym.Wrapper):
     def __init__(self, env, penalty_weight=0.1):
         super().__init__(env)
         self.penalty_weight = penalty_weight
 
-    def reward(self, reward):
-        x, y = self.env.unwrapped.state[0], self.env.unwrapped.state[1]
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        x, y = obs[0], obs[1]
         distance = np.sqrt(x**2 + y**2)
-        return reward - self.penalty_weight * distance
+        shaped_reward = reward - self.penalty_weight * distance
+        return obs, shaped_reward, terminated, truncated, info
